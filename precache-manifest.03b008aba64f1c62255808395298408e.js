d1a3bc6679fbde63ac637434e97b33cc90a630e6
iMervinC <itorma.mervin@gmail.com>
2018-10-30 22:09:52 +0800

    Updates

---
+++ precache-manifest.03b008aba64f1c62255808395298408e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "2026c35118f4e027fc83",
+    "url": "/contactmanager/static/js/main.2026c351.chunk.js"
+  },
+  {
+    "revision": "74607de25a3027ea0279",
+    "url": "/contactmanager/static/js/1.74607de2.chunk.js"
+  },
+  {
+    "revision": "2026c35118f4e027fc83",
+    "url": "/contactmanager/static/css/main.b3fa87a0.chunk.css"
+  },
+  {
+    "revision": "74607de25a3027ea0279",
+    "url": "/contactmanager/static/css/1.4af41854.chunk.css"
+  },
+  {
+    "revision": "0442c281967b8e127b2bc943fb362d5e",
+    "url": "/contactmanager/index.html"
+  }
+];
