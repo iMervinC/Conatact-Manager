c438cb8671c88a0ce3934f91eb832e9d29b28534
iMervinC <itorma.mervin@gmail.com>
2018-10-30 21:59:53 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
+import AddContacts from "./components/contacts/AddContacts";
+import EditContact from "./components/contacts/EditContact";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+
+import { Provider } from "./context";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContacts} />
+                <Route
+                  exact
+                  path="/contacts/edit/:id"
+                  component={EditContact}
+                />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
